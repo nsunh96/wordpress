dea2c1eb7706df158fe13c1f30c5cac18366252a
nsunh96 <nathan@nathansumner.com>
2016-11-21 12:11:49 -0500

    CSS and Script updates
    Updated basic CSS, updated CSS & JS to CDN

--- themes/resumeme/library/enqueue-scripts.php
+++ themes/resumeme/library/enqueue-scripts.php
@@ -4,13 +4,13 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	function foundationpress_scripts() {
 
 	// Enqueue Main Stylesheet
-	wp_enqueue_style( 'Main Stylesheet', get_stylesheet_directory_uri() . '/css/foundation.css' );
+	wp_enqueue_style( 'Foundation Stylesheet', 'https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.1/css/foundation.min.css' );
 		
 	// Enqueue WordPress Stylesheet
 	wp_enqueue_style( 'WordPress Stylesheet', get_stylesheet_directory_uri() . '/style.css' );
 	
 	// Enqueue Font Awesome Stylesheet
-	wp_enqueue_style( 'Font Awesome Stylesheet', get_stylesheet_directory_uri() . '/css/font-awesome.css' );
+	wp_enqueue_style( 'Font Awesome Stylesheet', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css' );
 	
 	// Enqueue ResumeMe Stylesheet
 	wp_enqueue_style( 'ResumeMe Stylesheet', get_stylesheet_directory_uri() . '/css/resumeme/resumeme.css' );
@@ -19,7 +19,7 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	wp_deregister_script( 'jquery' );
 
 	// Modernizr is used for polyfills and feature detection. Must be placed in header. (Not required)
-	wp_register_script( 'modernizr', get_template_directory_uri() . '/js/vendor/modernizr.js', array(), '2.8.3', false );
+	wp_register_script( 'modernizr', 'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js', array(), '2.8.3', false );
 
 	// Fastclick removes the 300ms delay on click events in mobile environments. Must be placed in header. (Not required)
 	wp_register_script( 'fastclick', get_template_directory_uri() . '/js/vendor/fastclick.js', array(), '1.0.0', false );
@@ -30,12 +30,8 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
 	wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
 
-	// Self hosted jQuery placed in the footer. (Comment the script above and uncomment the script below if you want to switch).
-	//wp_register_script( 'jquery', get_template_directory_uri() . '/js/vendor/jquery.js', array(), '2.1.3', true );
-
-	// If you'd like to cherry-pick the foundation components you need in your project, head over to Gruntfile.js and see lines 67-88
-	// It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
-	wp_register_script( 'foundation', get_template_directory_uri() . '/js/foundation.js', array('jquery'), '5.5.1', true );
+	// Foundation JS
+	wp_register_script( 'foundation', 'https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.1/js/foundation.min.js', array('jquery'), '5.5.1', true );
 
 	// Enqueue all registered scripts
 	wp_enqueue_script( 'modernizr' );
