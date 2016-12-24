e119f05bbcbc49be6649b427e0c1b3b75794cc79
nsunh96 <nathan@nathansumner.com>
2016-12-24 03:16:16 -0500

    BJG Electronics Theme
    Initial Push

---
+++ themes/bjg-electronics/footer.php
@@ -0,0 +1,13 @@
+		<div class="footer">
+			<div class="container">
+				<div class="row">
+					<?php dynamic_sidebar( 'footer-widgets' ); ?>
+				</div>
+			</div>
+		</div>
+		<footer>
+        	<p class="text-center">Copyright &copy; 2015 <?php bloginfo('name'); ?>. All Rights Reserved</p>
+     	</footer>
+    <?php wp_head(); ?>
+	</body>
+</html>
